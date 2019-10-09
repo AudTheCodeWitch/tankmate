9f0f3fb09140453bd2c0b192e384923a9a1b417e
Audrea Cook <audreacwork@gmail.com>
2019-10-09 15:34:57 -0600

    Fix omniauth params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def home
-
+    @user = current_user
   end
 
   def after_sign_in_path_for(resource)
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.username = auth.info.name
       user.password = Devise.friendly_token[0,20]
     end
   end
