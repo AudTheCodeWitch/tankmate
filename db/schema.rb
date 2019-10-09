498e2643263b4586c7bb59331f2f0aabc310c7ea
Audrea Cook <audreacwork@gmail.com>
2019-10-09 16:44:50 -0600

    Add tank table & associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_203542) do
+ActiveRecord::Schema.define(version: 2019_10_09_224057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tanks", force: :cascade do |t|
+    t.string "name"
+    t.integer "size"
+    t.text "description"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
