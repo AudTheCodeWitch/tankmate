575c76a49fac9ffe39bde8703c2c8524a047c9bc
Audrea Cook <audreacwork@gmail.com>
2019-10-10 11:17:19 -0600

    Add default images for fish and plants

--- db/migrate/20191010152334_create_fish.rb
+++ db/migrate/20191010152334_create_fish.rb
@@ -2,7 +2,7 @@ class CreateFish < ActiveRecord::Migration[6.0]
   def change
     create_table :fish do |t|
       t.string :species
-      t.string :image
+      t.string :image, default: 'public/images/fish.png'
       t.references :tank, null: false, foreign_key: true
 
       t.timestamps
--- db/migrate/20191010153346_create_plants.rb
+++ db/migrate/20191010153346_create_plants.rb
@@ -2,7 +2,7 @@ class CreatePlants < ActiveRecord::Migration[6.0]
   def change
     create_table :plants do |t|
       t.string :species
-      t.string :image
+      t.string :image, default: 'public/images/plant.jpg'
       t.references :tank, null: false, foreign_key: true
 
       t.timestamps
