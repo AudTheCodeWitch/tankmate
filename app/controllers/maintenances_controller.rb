ab22317ef82d8c54fc433962d16d2345d73667d2
Audrea Cook <audreacwork@gmail.com>
2019-10-11 15:09:50 -0600

    Write maintenance index method

--- app/controllers/maintenances_controller.rb
+++ app/controllers/maintenances_controller.rb
@@ -1,6 +1,7 @@
 class MaintenancesController < ApplicationController
   def index
-    @maintenances = Maintenance.all
+    @user = User.find(params[:user_id])
+    @maintenances = @user.maintenances
   end
 
   def show
