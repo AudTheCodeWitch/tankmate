85fbe28d352cb6088aa33c680f123936ec9ea7c8
Audrea Cook <audreacwork@gmail.com>
2019-10-11 10:33:46 -0600

    Nest fish and plant routes under tanks

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :plants
-  resources :fish
   root to: 'application#home'
   devise_for :users, :controllers => { registrations: 'registrations', :omniauth_callbacks => "users/omniauth_callbacks" }
-  resources :tanks, only: [:index]
+  resources :tanks, only: [:index] do
+    resources :fish, only: [:new, :create, :update, :destroy, :edit]
+    resources :plants, only: [:new, :create, :update, :destroy, :edit]
+  end
   resources :users, only: [:show] do
     resources :tanks, only: [:new, :create, :show, :update, :destroy, :edit]
   end
