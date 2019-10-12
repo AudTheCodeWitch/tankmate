0ad903b2defd9d1c9a2f28d0dffc38060ff4bff2
Audrea Cook <audreacwork@gmail.com>
2019-10-12 15:28:44 -0600

    Wire up form

--- app/controllers/maintenances_controller.rb
+++ app/controllers/maintenances_controller.rb
@@ -8,4 +8,43 @@ class MaintenancesController < ApplicationController
     @maintenance = Maintenance.find(params[:id])
   end
 
+  def new
+    @maintenance = Maintenance.new
+  end
+
+  def create
+    maintenance_params[:tank_id].each do |t|
+      unless t.blank?
+        @maintenance = Maintenance.new(maintenance_params)
+        @maintenance.tank_id = t
+        @maintenance.task_id = set_task(maintenance_params)
+        render :new unless @maintenance.save
+      end
+    end
+
+    redirect_to user_maintenances_path(current_user)
+  end
+
+  private
+
+  def maintenance_params
+    params.require(:maintenance).permit(
+        :due,
+        :complete,
+        :notes,
+        :task_id,
+        tank_id: [],
+        :task_attributes => :title
+      )
+  end
+
+  def set_task(maintenance_params)
+    if maintenance_params[:task_id].blank?
+      t = Task.find_or_create_by(title: maintenance_params[:task_attributes][:title].capitalize)
+      t.id
+    else
+      maintenance_params[:task_id]
+    end
+  end
+
 end
--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -1,4 +1,7 @@
 class Maintenance < ApplicationRecord
   belongs_to :task
   belongs_to :tank
+  accepts_nested_attributes_for :task
+
+
 end
