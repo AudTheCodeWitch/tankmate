7fad95256ea19714847ea230abe91c581f98c63f
Audrea Cook <audreacwork@gmail.com>
2019-10-12 00:42:13 -0600

    Remove CRUD methods

--- app/controllers/maintenances_controller.rb
+++ app/controllers/maintenances_controller.rb
@@ -8,38 +8,4 @@ class MaintenancesController < ApplicationController
     @maintenance = Maintenance.find(params[:id])
   end
 
-  def new
-    @maintenance = Maintenance.new
-  end
-
-  def create
-    @maintenance = Maintenance.new(maintenance_params)
-    if @maintenance.save
-      redirect_to user_maintenances_path(current_user)
-    else
-      render :new
-    end
-
-  end
-
-  def edit
-
-  end
-
-  def update
-
-  end
-
-  def destroy
-    @maintenance = Maintenance.find(params[:id])
-    @maintenance.destroy
-    redirect_to user_maintenances_path(current_user)
-  end
-
-  private
-
-  def maintenance_params
-    params.require(:maintenance).permit(:due, :complete?, :notes, :tank_id, :task_id)
-  end
-
 end
