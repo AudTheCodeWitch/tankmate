be10328307506a4563f6adfcdbceb040dc8056ac
Audrea Cook <audreacwork@gmail.com>
2019-10-13 12:56:13 -0600

    Add tank validations

--- app/models/tank.rb
+++ app/models/tank.rb
@@ -4,4 +4,8 @@ class Tank < ApplicationRecord
   has_many :plants
   has_many :maintenances
   has_many :tanks, through: :maintenances
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  validates :size, numericality: { greater_than: 0 }
 end
