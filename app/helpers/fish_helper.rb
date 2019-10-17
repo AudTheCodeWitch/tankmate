6696a859906c8f22ed61f95ce7b299de7f3a85ee
Audrea Cook <audreacwork@gmail.com>
2019-10-17 15:13:22 -0600

    Add #owners_list and #tank_list helper methods

--- app/helpers/fish_helper.rb
+++ app/helpers/fish_helper.rb
@@ -1,2 +1,21 @@
 module FishHelper
+
+  def owners_list(fish)
+    list = fish.collect do |f|
+      f.tank.user
+    end
+    list.uniq
+  end
+
+  def tank_list(owner, fish)
+    list = []
+    fish.each do |f|
+      if f.tank.user_id == owner.id
+        list << f.tank
+      end
+    end
+    list.uniq
+  end
+
+
 end
